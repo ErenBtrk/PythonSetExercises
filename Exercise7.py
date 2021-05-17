792cc049947c49b9c5aa5a01c819366d41cfd23d
ErenBtrk <erenbtrk@hotmail.com>
2021-05-17 11:43:54 +0300

    Exercise 7

---
+++ Exercise7.py
@@ -0,0 +1,14 @@
+'''
+7. Write a Python program to create a union of sets.
+'''
+
+set1 = set([1,2,3,4,5])
+set2 = set([6,7,8])
+set3 = set1 | set2
+print(set3)
+
+#Union
+setx = set(["green", "blue"])
+sety = set(["blue", "yellow"])
+seta = setx | sety
+print(seta)
